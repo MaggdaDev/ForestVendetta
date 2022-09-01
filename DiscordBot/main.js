531e2dc9c2c1d3a68903806b078a3864b2f435d3
MaggdaDev <davidbetko05@gmail.com>
2022-09-01 17:26:23 +0200

    serious bug fix with feetpic command

--- DiscordBot/main.js
+++ DiscordBot/main.js
@@ -6,12 +6,12 @@ const CommandManager = require('./src/Commands/commandManager');
 const ForestScout = require('./src/ForestScout');
 
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 
 const forestScout = new ForestScout(token, client);
 
 // When the client is ready, run this code (only once)
 client.once('ready', ()=>forestScout.onReady(forestScout));
 client.on('interactionCreate', (interaction)=>forestScout.onInteraction(forestScout, interaction));
-client.on('message', (message)=>forestScout.onMessage(forestScout, message));
+client.on('messageCreate', (message)=>forestScout.onMessage(forestScout, message));
 client.login(token);
