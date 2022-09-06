33f0855a57ad00aaeac0b5bf0523e06dc64114d4
MaggdaDev <davidbetko05@gmail.com>
2022-09-06 20:23:36 +0200

    first amqlafhaibfihbf attempt

--- Scheduler/script/main.js
+++ Scheduler/script/main.js
@@ -1,3 +1,8 @@
-const amqp = require('../../node_modules/amqplib');
+const amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://192.168.137.1');
+amqp.connect('amqp://192.168.137.1', (error0, connection)=>{
+    if(error0) throw error0;
+    connection.createChannel(error1, channel)=>{
+        if(error1) throw
+    });
+});
