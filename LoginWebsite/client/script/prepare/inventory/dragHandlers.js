59f223b0f5422c9b124da513e929818971136da0
MaggdaDev <davidbekot05@gmail.com>
2023-01-28 23:35:16 +0100

    authtua okbubko

--- LoginWebsite/client/script/prepare/inventory/dragHandlers.js
+++ LoginWebsite/client/script/prepare/inventory/dragHandlers.js
@@ -3,3 +3,11 @@ function onItemFrameDragStarted(event) {
     const id = srcElement.id;
     console.log(id);
 }
+
+function onItemFrameDroppedToHotbar(event) {
+    const srcElement = event.srcElement;
+    const id = srcElement.id;
+    console.log(id);
+
+    document.getElementById("hotbarDiv").appendChild(document.getElementById(id));
+}
